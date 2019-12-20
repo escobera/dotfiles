2eef4435756a245131de212b9c31b874a407d971
Rafael Barbosa <rbocosta@gmail.com>
2019-12-20 19:01:10 -0300

    Overhauling the dotfiles installer

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,31 +1,98 @@
-#!/bin/zsh
-echo "Backing up old vim to ~/vim-backup"
-mv ~/.vim ~/vim-backup 2> /dev/null
-cp ~/.vimrc ~/.vimrc.old 2> /dev/null
-rm ~/.vimrc 2> /dev/null
+#!/usr/bin/env bash
+TITLE='\033[0;36m'
+NC='\033[0m'
 
-echo "Creating vim directories"
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/tmp
-cd ~/.vim
-mkdir -p bundle
+print_step () {
+  echo -e "${TITLE}##### $1 #####${NC}"
+}
 
-echo "Downloading vundle"
-git clone http://github.com/gmarik/vundle.git bundle/vundle
+install_source_code_pro () {
+  cd Downloads
 
-echo "Download/Install prezto"
-git clone --recursive https://github.com/escobera/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-chsh -s /bin/zsh
+  wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
 
-echo "Linking stuffz..."
-ln -s ~/projects/dotfiles/vimrc ~/.vimrc
-ln -s ~/projects/dotfiles/gemrc ~/.gemrc
-ln -s ~/projects/dotfiles/pryrc ~/.pryrc
-# ln -s ~/projects/dotfiles/prompt_escobera_setup  ~/.zprezto/modules/prompt/functions
-# ln -s '~/projects/dotfiles/Default (Linux).sublime-keymap'  '~/.config/sublime-text-2/Packages/User/Default (Linux).sublime-keymap'
-# ln -s '~/projects/dotfiles/Preferences.sublime-settings'  '~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings'
-# ln -s '~/projects/dotfiles/DeleteLine.sublime-macro'  '~/.config/sublime-text-2/Packages/User/DeleteLine.sublime-macro'
+  if [ ! -d "~/.fonts" ] ; then
+      mkdir ~/.fonts
+  fi
+
+  unzip 1.050R-it.zip 
+
+  cp source-code-pro-*-it/OTF/*.otf ~/.fonts/
+  rm -rf source-code-pro* 
+  rm 1.050R-it.zip 
+
+  cd ~/
+
+  fc-cache -f -v
+}
+
+install_asdf () {
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.6
+  . $HOME/.asdf/asdf.sh
+  . $HOME/.asdf/completions/asdf.bash
+  
+  asdf plugin add erlang
+  asdf plugin add elixir
+  asdf plugin add nodejs
+  asdf plugin add ruby
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+  asdf install ruby 2.6.4
+  asdf global ruby 2.6.4
+
+  asdf install erlang 22.2.1
+  asdf global erlang 22.2.1
+
+  asdf install elixir 1.9.4
+  asdf global elixir 1.9.4
+  
+  asdf install nodejs 12.14.0
+  asdf global nodejs 12.14.0
+}
+
+print_step "Installing dependencies"
+if [ ! -f /usr/bin/curl ] ; then
+  sudo apt update
+  sudo apt install --assume-yes zsh git vim automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev \
+  unzip curl
+fi
+
+print_step "Installing Source Code Pro Font"
+if [ ! -f ~/.fonts/SourceCodePro-Regular.otf ] ; then
+ install_source_code_pro
+fi
+
+print_step "Installing ZimFW"
+if [ ! -d $HOME/.zim ] ; then
+  touch ~/.zshrc
+  /bin/zsh <<'EOF'
+    git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
+    for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/*; do
+      user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+      cat ${template_file} ${user_file}(.N) > ${user_file}.tmp && mv ${user_file}{.tmp,}
+    done
+EOF
+  chsh -s /bin/zsh
+
+  print_step "Adding my .zshrc and .zimrc"
+  rm ~/.zimrc
+  rm ~/.zshrc
+  mv ./configs/zimrc ~/.zimrc
+  mv ./configs/zshrc ~/.zshrc
+fi
+
+print_step "Install ASDF and plugins"
+if [ ! -d $HOME/.asdf ] ; then
+  install_asdf
+fi
+
+print_step "Install dracula color scheme"
+ 
+
+print_step "Fix TILIX configs"
+if [ ! -f /etc/profile.d/vte.sh ] ; then
+  sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+fi
+
